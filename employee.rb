ef7aef33100c12adcc675fb834bdcf2254352214
Peter Jang <peter.x.jang@gmail.com>
2020-02-24 19:41:32 -0600

    Represent employees using hashes with symbol keys

--- employee.rb
+++ employee.rb
@@ -9,3 +9,9 @@ employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 800
 employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }
 puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
 puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} a year."
+
+# Represent employees as a hash with symbols for keys
+employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true }
+employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
+puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
