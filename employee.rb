79ba641eba62e82daf47b65f79564b99d5640f59
Peter Jang <peter.x.jang@gmail.com>
2020-02-24 19:21:55 -0600

    Represent employees as arrays

--- employee.rb
+++ employee.rb
@@ -1,1 +1,5 @@
-
+# Represent employees as an array
+employee1 = ["Majora", "Carter", 80000, true]
+employee2 = ["Danilo", "Campos", 70000, true]
+puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
