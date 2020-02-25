457ba5ce51ccceee878a414de1b7e3e7838e69f7
Peter Jang <peter.x.jang@gmail.com>
2020-02-24 19:56:16 -0600

    Represent employees using class instances

--- employee.rb
+++ employee.rb
@@ -15,3 +15,22 @@ employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000,
 employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
 puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
 puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+
+# Represent employees as a class instance
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
+employee2 = Employee.new("Danilo", "Campos", 70000, true)
+employee1.print_info
+employee2.print_info
