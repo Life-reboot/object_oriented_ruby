6309bfb40e00b9955fb498d67f16f79ac7a1e810
Peter Jang <peter.x.jang@gmail.com>
2020-02-25 19:25:06 -0600

    Add manager class using inheritance

---
+++ manager.rb
@@ -0,0 +1,36 @@
+class Employee
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
+employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
+employee1.print_info
+employee2.print_info
+
+class Manager < Employee
+  def send_report
+    puts "Sending email..."
+    # use email sending library
+    puts "Email sent!"
+  end
+end
+
+manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true)
+manager.print_info
+manager.send_report
