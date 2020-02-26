e0baaecae85f87c3f0a48bfe2ef7d949be270bce
Peter Jang <peter.x.jang@gmail.com>
2020-02-25 21:29:33 -0600

    Add manager methods to give raises and fire all employees

--- manager.rb
+++ manager.rb
@@ -34,9 +34,35 @@ class Manager < Employee
     # use email sending library
     puts "Email sent!"
   end
+
+  def give_all_raises
+    puts "Gonna give all raises"
+
+    # index = 0
+    # while index < @employees.length
+    #   employee = @employees[index]
+    #   employee.give_annual_raise
+    #   index += 1
+    # end
+
+    @employees.each do |employee|
+      employee.give_annual_raise
+    end
+  end
+
+  def fire_all_employees
+    @employees.each do |employee|
+      employee.active = false
+    end
+  end
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
 manager.print_info
 manager.send_report
+p manager
+manager.give_all_raises
+employee1.print_info
+employee2.print_info
+manager.fire_all_employees
 p manager
