1f57f49701be431ba2ffcae8415f2a6c5d59aad7
Peter Jang <peter.x.jang@gmail.com>
2020-02-26 18:38:05 -0600

    Add car/bike module example

---
+++ module_example.rb
@@ -0,0 +1,47 @@
+module Moveable
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include Moveable
+
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include Moveable
+
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car = Car.new
+p car
+car.honk_horn
+
+bike = Bike.new
+p bike
+bike.ring_bell
