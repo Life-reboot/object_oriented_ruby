667c77f24f550caaa52b97ac52d2e09d0bfcbf84
Peter Jang <peter.x.jang@gmail.com>
2020-02-26 18:50:28 -0600

    Split single file into separate files using require

--- employee.rb
+++ employee.rb
@@ -1,22 +1,3 @@
-# Represent employees as an array
-employee1 = ["Majora", "Carter", 80000, true]
-employee2 = ["Danilo", "Campos", 70000, true]
-puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-
-# Represent employees as a hash
-employee1 = { "first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true }
-employee2 = { "first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true }
-puts "#{employee1["first_name"]} #{employee1["last_name"]} makes #{employee1["salary"]} a year."
-puts "#{employee2["first_name"]} #{employee2["last_name"]} makes #{employee2["salary"]} a year."
-
-# Represent employees as a hash with symbols for keys
-employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true }
-employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
-puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-
-# Represent employees as a class instance
 class Employee
   attr_reader :first_name, :last_name, :active
   attr_writer :active
@@ -36,15 +17,3 @@ class Employee
     @salary = 1.05 * @salary
   end
 end
-
-employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
-p employee1.first_name
-p employee1.last_name
-p employee1.active
-employee1.active = false
-p employee1.active
--- manager.rb
+++ manager.rb
@@ -1,50 +1,15 @@
-class Employee
-  attr_reader :first_name, :last_name, :active
-  attr_writer :active
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @salary = input_options[:salary]
-    @active = input_options[:active]
-  end
-
-  def print_info
-    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
-  end
-
-  def give_annual_raise
-    @salary = 1.05 * @salary
-  end
-end
-
-employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
-employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
-employee1.print_info
-employee2.print_info
+require "./employee.rb"
+require "./email_reportable.rb"
 
 class Manager < Employee
+  include EmailReportable
+
   def initialize(input_options)
     super(input_options)
     @employees = input_options[:employees]
   end
 
-  def send_report
-    puts "Sending email..."
-    # use email sending library
-    puts "Email sent!"
-  end
-
   def give_all_raises
-    puts "Gonna give all raises"
-
-    # index = 0
-    # while index < @employees.length
-    #   employee = @employees[index]
-    #   employee.give_annual_raise
-    #   index += 1
-    # end
-
     @employees.each do |employee|
       employee.give_annual_raise
     end
@@ -56,13 +21,3 @@ class Manager < Employee
     end
   end
 end
-
-manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
-manager.print_info
-manager.send_report
-p manager
-manager.give_all_raises
-employee1.print_info
-employee2.print_info
-manager.fire_all_employees
-p manager
